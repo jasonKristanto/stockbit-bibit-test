a880d8e7c7e679da0d9e27de314f6a29407be789
jasonkristanto <jasonkristanto4@gmail.com>
2021-06-19 12:15:50 +0700

    Initial answer for anagram problem

--- src/anagram/index.js
+++ src/anagram/index.js
@@ -1,1 +1,24 @@
-console.log('hello anagram');
+function anagram(arrWords) {
+  const ans = [];
+  const arrAnagramWords = [];
+  let counterArrAnagramWords = 0;
+
+  arrWords.forEach((word) => {
+    let anagramWord = word.split('');
+    anagramWord = anagramWord.sort();
+    anagramWord = anagramWord.join('');
+
+    if (!(anagramWord in arrAnagramWords)) {
+      arrAnagramWords[anagramWord] = counterArrAnagramWords;
+      counterArrAnagramWords += 1;
+
+      ans[arrAnagramWords[anagramWord]] = [word];
+    } else {
+      ans[arrAnagramWords[anagramWord]].push(word);
+    }
+  });
+
+  return ans;
+}
+
+console.log(anagram(['kita', 'atik', 'tika', 'aku', 'kia', 'makan', 'kua', 'makam', 'akan', 'kodok', 'aki', 'apel', 'elap', 'peal']));
