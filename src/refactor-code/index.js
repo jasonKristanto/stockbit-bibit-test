62e62f4f6405caa0398feb1cb4055388479fea5e
jasonkristanto <jasonkristanto4@gmail.com>
2021-06-19 11:56:09 +0700

    Initial answers to refactor problem and sql problem

--- src/refactor-code/index.js
+++ src/refactor-code/index.js
@@ -1,1 +1,18 @@
-console.log('hello refactor-code');
+function findFirstStringInBracket(str) {
+  if (str.length > 0 && str.indexOf('(') >= 0) {
+    const indexFirstBracketFound = str.indexOf('(');
+    const indexClosingBracketFound = str.indexOf(')');
+
+    if (indexFirstBracketFound >= 0 && indexClosingBracketFound > indexFirstBracketFound) {
+      return str.substring(indexFirstBracketFound + 1, indexClosingBracketFound);
+    }
+  }
+
+  return '';
+}
+
+console.log(`ans: ${findFirstStringInBracket('(halo semuanya) (hehe)')}`); console.log('');
+console.log(`ans: ${findFirstStringInBracket('halo semuanya')}`); console.log('');
+console.log(`ans: ${findFirstStringInBracket('(halo semuanya')}`); console.log('');
+console.log(`ans: ${findFirstStringInBracket('()')}`); console.log('');
+console.log(`ans: ${findFirstStringInBracket('(i)')}`); console.log('');
